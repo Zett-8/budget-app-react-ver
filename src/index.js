3eca3eef56ffbb42f6e1ce4829c60793fbf34184
Toshiki Kamei <kame0008@gmail.com>
2018-07-06 13:58:45 +0900

    fixed

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import rootReducer from './reducers';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 
-let store = createStore(rootReducer);
+let store = createStore(rootReducer,window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
     <Provider store={store}>
